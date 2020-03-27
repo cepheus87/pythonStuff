fc2c94d45e267f4dafca8fcaadbb08bc4042b579
tomasz <mr.tomasz.bednarski@gmail.com>
2020-03-27 08:17:13 +0100

    pythonpath update

--- pythonpath_in_venv.py
+++ pythonpath_in_venv.py
@@ -1,3 +1,6 @@
+# https://stackoverflow.com/questions/4757178/how-do-you-set-your-pythonpath-in-an-already-created-virtualenv/17963979#17963979
+# add export PYTHONPATH="/the/path/you/want" in /bin/activate
+
 import os
 try:
         user_paths = os.environ['PYTHONPATH'].split(os.pathsep)
